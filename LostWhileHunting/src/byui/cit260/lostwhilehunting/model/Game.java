c8189f1da2b661f59270a2a9e90ebf37572b2340
tlove21 <tlove21@gmail.com>
2017-10-04 13:22:46 -0500

    4/10/2017 11:34AM - Created a local Project for the GitHUb implementation

---
+++ LostWhileHunting/src/byui/cit260/lostwhilehunting/model/Game.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lostwhilehunting.model;
+
+import java.io.Serializable;
+/**
+ *
+ * @author Administrator
+ */
+public class Game implements Serializable{
+    
+    //class instance variables
+    private double totalScore;
+    private int numberOfSaves;
+    private long totalTime;
+    private Player player;
+
+    public Game() {
+    }
+
+    public double getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(double totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public int getNumberOfSaves() {
+        return numberOfSaves;
+    }
+
+    public void setNumberOfSaves(int numberOfSaves) {
+        this.numberOfSaves = numberOfSaves;
+    }
+
+    public long getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(long totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    // Player Class getter and setter functions
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + (int) (Double.doubleToLongBits(this.totalScore) ^ (Double.doubleToLongBits(this.totalScore) >>> 32));
+        hash = 41 * hash + this.numberOfSaves;
+        hash = 41 * hash + (int) (this.totalTime ^ (this.totalTime >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (Double.doubleToLongBits(this.totalScore) != Double.doubleToLongBits(other.totalScore)) {
+            return false;
+        }
+        if (this.numberOfSaves != other.numberOfSaves) {
+            return false;
+        }
+        if (this.totalTime != other.totalTime) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "totalScore=" + totalScore + ", numberOfSaves=" + numberOfSaves + ", totalTime=" + totalTime + '}';
+    }
+    
+    
+    
+}
---
+++ LostWhileHunting/src/byui/cit260/lostwhilehunting/model/Player.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lostwhilehunting.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Player implements Serializable{
+    
+    //class instance variables
+    private String name;
+    private int numberOfSurePasses;
+    private ArrayList<Game> games = new ArrayList<Game>();
+    
+    public Player() {
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfSurePasses() {
+        return numberOfSurePasses;
+    }
+
+    public void setNumberOfSurePasses(int numberOfSurePasses) {
+        this.numberOfSurePasses = numberOfSurePasses;
+    }
+    
+    //Game Class getter and setter methods
+
+    public ArrayList<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(ArrayList<Game> games) {
+        this.games = games;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.name);
+        hash = 41 * hash + this.numberOfSurePasses;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (this.numberOfSurePasses != other.numberOfSurePasses) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", numberOfSurePasses=" + numberOfSurePasses + '}';
+    }
+
+    
+    
+    
+}
