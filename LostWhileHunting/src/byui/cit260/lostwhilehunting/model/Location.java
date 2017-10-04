5ad1d4ec100a8144a89a867796c8b878efefffca
tlove21 <tlove21@gmail.com>
2017-10-04 14:43:22 -0500

    4/10/2017 11:34AM - Created a local Project for the GitHUb implementation

---
+++ LostWhileHunting/src/byui/cit260/lostwhilehunting/model/Location.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lostwhilehunting.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Location implements Serializable{
+    
+    //class instance variables
+    private double column;
+    private double row;
+    private boolean visited;
+    private String attribute;
+    private ArrayList<Actors> actors = new ArrayList<Actors>();
+    private Map map;
+
+    //Public Functions for Location
+
+    public Location() {
+        map = new Map();
+    }
+
+    public double getColumn() {
+        return column;
+    }
+
+    public void setColumn(double column) {
+        this.column = column;
+    }
+
+    public double getRow() {
+        return row;
+    }
+
+    public void setRow(double row) {
+        this.row = row;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+    
+    //Actors Class Getter and Setter functions
+    
+     public ArrayList<Actors> getActors() {
+        return actors;
+    }
+
+    public void setActors(ArrayList<Actors> actors) {
+        this.actors = actors;
+    }
+    
+    //Class Hash, Equals, and toString override functions
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.column) ^ (Double.doubleToLongBits(this.column) >>> 32));
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.row) ^ (Double.doubleToLongBits(this.row) >>> 32));
+        hash = 71 * hash + (this.visited ? 1 : 0);
+        hash = 71 * hash + Objects.hashCode(this.attribute);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (Double.doubleToLongBits(this.column) != Double.doubleToLongBits(other.column)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.row) != Double.doubleToLongBits(other.row)) {
+            return false;
+        }
+        if (this.visited != other.visited) {
+            return false;
+        }
+        if (!Objects.equals(this.attribute, other.attribute)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "column=" + column + ", row=" + row + ", visited=" + visited + ", attribute=" + attribute + '}';
+    }
+    
+    
+    
+}
