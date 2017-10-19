90fef001a1d24fb9ad944ee792be1dbf7817579e
tlove21 <tlove21@gmail.com>
2017-10-19 17:10:24 -0500

    18/10/2017 - ItemControl fixed

--- LostWhileHunting/src/byui/cit260/lostwhilehunting/control/LocationsControl.java
+++ LostWhileHunting/src/byui/cit260/lostwhilehunting/control/LocationsControl.java
@@ -9,6 +9,61 @@ package byui.cit260.lostwhilehunting.control;
  *
  * @author Group
  */
+
+import byui.cit260.lostwhilehunting.control.ItemControl;
+
 public class LocationsControl {
+    
+    public static double incrementLocationsMapSceneTypeIfSurePass(int surePass){
+        
+        //Declare Variables
+        String inventoryItem = "";
+        double randomizerNum = 0;
+        double returnSuccessFailure= 0;
+        
+        // This if statement checks the surePass for amount
+        if(surePass != 0){
+            
+            //This generates a random number between 1 and 100
+            randomizerNum = Math.random()*100;
+           
+            /* this if statement gives an 80% chance of success, 
+            but if it fails you still get an item making it 100% success.*/
+            if(randomizerNum <= 80){
+                
+                /* The moveToNextLocation function will increment the locations 
+                and decrease the scenes between each main location.*/
+                //moveToNextLocation(); This function has not been written yet
+                
+                System.out.println("Congrats you have managed to bypass the danger successfully.");
+                surePass = surePass - 1; // decrease surePass by 1
+                returnSuccessFailure = 1;// assign 1 for test class
+                
+            }else{
+                
+                surePass = surePass - 1; // decrease surePass by 1
+                
+                //This item will be written to the SetInventoryItem function in future iterations
+                inventoryItem = ItemControl.generateItemFromItems();
+                System.out.println("Your Sure Pass failed this time, but you found a " + inventoryItem);
+                returnSuccessFailure = 0;// assign 0 for test class
+                
+            }
+            
+        }else if(surePass < 0 || surePass > 4){ //Checks for anomalies
+            
+            System.out.println("Hacking attempt detected or Major Catastrophy detected.");
+            returnSuccessFailure = 99999; // assign 99999 for test class
+            
+        }
+        else{
+            
+            System.out.println("You have no Sure Passes left sadly, be careful on your journey");
+            returnSuccessFailure = 99999; // assign 99999 for test class
+            
+        }
+        
+        return returnSuccessFailure;
+    }
     
 }
