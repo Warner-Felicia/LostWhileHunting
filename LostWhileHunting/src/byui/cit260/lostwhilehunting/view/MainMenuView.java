774aa268768db754cd4e77ff1cb279061d58d84e
fiorald <fiorald10@gmail.com>
2017-10-26 21:02:59 +0200

    Merge origin/master
    Conflicts:
    	LostWhileHunting/src/byui/cit260/lostwhilehunting/control/LocationsControl.java

--- LostWhileHunting/src/byui/cit260/lostwhilehunting/view/MainMenuView.java
+++ LostWhileHunting/src/byui/cit260/lostwhilehunting/view/MainMenuView.java
@@ -5,14 +5,104 @@
  */
 package byui.cit260.lostwhilehunting.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author New User
  */
 public class MainMenuView {
+    
+    private String menu;
 
-    void displayMainMenuView() {
-        System.out.println("\n*** displayMenu() function called ***");
+    public MainMenuView() {
+        this.menu = "\n"
+                    + "\n----------------------------------------"
+                    + "\n| Main Menu                            |"
+                    + "\n----------------------------------------"
+                    + "\nN - Start new game"
+                    + "\nG - Get and start saved game"
+                    + "\nH - Get help on how to play the game"
+                    + "\nS - Save game"
+                    + "\nQ - Quit"
+                    + "\n----------------------------------------";
     }
+    void displayMainMenuView() {
+        
+        boolean done = false; // set flag to not done
+        do {
+            // promt for and get players name
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q")) // user wants to quit
+                return; // exit the game
+            
+            // do the requested action and display the next view
+            done = this.doAction(menuOption);
+        } while (!done);
+    }
+
+    private String getMenuOption() {
+       Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+        
+        while (!valid) {
+            System.out.println("\n" + this.menu);
+            
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            if (value.length() < 1) {
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+            }
+            
+            break; // end the loop
+        }
+        
+        return value;  // return the value entered
+    }
+
+    private boolean doAction(String choice) {
+       
+        choice = choice.toUpperCase(); // convert choice to upper case
+        
+        switch (choice) {
+            case "N": // create and start a new game
+                this.startNewGame();
+                break;
+            case "G": // get and start an existing game
+                this.startExistingGame();
+                break;
+            case "H": // display the help menu
+                this.displayHelpMenu();
+                break;
+            case "S": // save the current game
+                this.saveGame();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        
+        return false;
+    }
+
+    private void startNewGame() {
+        System.out.println("*** startNewGame function called ***");
+    }
+
+    private void startExistingGame() {
+        System.out.println("*** startExistingGame function called ***");
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("*** startExistingGame function called ***");
+    }
+
+    private void saveGame() {
+        System.out.println("** displayHelpMenu function called ***");
+    }
+    
     
 }
