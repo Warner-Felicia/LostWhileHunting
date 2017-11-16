7e43f6f2cfee63f1690cf973fcb7597e70854a88
tlove21 <tlove21@gmail.com>
2017-11-16 11:58:18 -0500

    16/11/2017 - Map Class

--- LostWhileHunting/src/byui/cit260/lostwhilehunting/model/Map.java
+++ LostWhileHunting/src/byui/cit260/lostwhilehunting/model/Map.java
@@ -13,15 +13,15 @@ import java.util.Objects;
  * Map Class
  * @author Trevon Morris
  */
-public enum Map implements Serializable{
+public class Map implements Serializable{
     
-    Start("Initial Cave",0,0),
+    /*Start("Initial Cave",0,0),
     Stream("Cool Stream",3,3),
     Cave("Dark Cave",6,6),
     Caverns("Deep Caverns",9,9),
     River("Great River",12,12),
     Town("Small Town",15,15),
-    Goal("Oregon Trail",18,18);
+    Goal("Oregon Trail",18,18);*/
     
     //class instance variables
     private String description;
@@ -31,25 +31,32 @@ public enum Map implements Serializable{
     
     //Public functions of Map Class
 
-    Map(String description, int columnCount, int rowCount) {
-        this.description = description;
-        this.columnCount = columnCount;
-        this.rowCount = rowCount;
+    public Map() {
     }
 
     public String getDescription() {
         return description;
     }
 
-    public double getColumnCount() {
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getColumnCount() {
         return columnCount;
     }
-   
-    public double getRowCount() {
+
+    public void setColumnCount(int columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    public int getRowCount() {
         return rowCount;
     }
-        
-    // Game Class getter and setter funtions
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
 
     public ArrayList<Game> getGames() {
         return games;
@@ -59,12 +66,58 @@ public enum Map implements Serializable{
         this.games = games;
     }
 
-    //Override To String
-    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.description);
+        hash = 23 * hash + this.columnCount;
+        hash = 23 * hash + this.rowCount;
+        hash = 23 * hash + Objects.hashCode(this.games);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (this.columnCount != other.columnCount) {
+            return false;
+        }
+        if (this.rowCount != other.rowCount) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.games, other.games)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize(); //To change body of generated methods, choose Tools | Templates.
+    }
+
     @Override
     public String toString() {
-        return "Map{" + "description=" + description + ", columnCount=" + columnCount + ", rowCount=" + rowCount + ", games=" + games + '}';
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
     }
-        
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
     
 }
