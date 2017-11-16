ba61c020e3e9de8cb3688b9120016c9ca754ad79
tlove21 <tlove21@gmail.com>
2017-11-16 11:14:37 -0500

    4/11/2017 - inventoryItem

---
+++ LostWhileHunting/src/byui/cit260/lostwhilehunting/model/InventoryItem.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lostwhilehunting.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Administrator
+ */
+public class InventoryItem {
+    
+    private String inventoryType;
+    private int quantityInStock;
+    private int requiredAmount;
+
+    public InventoryItem() {
+    }
+
+    public String getInventoryType() {
+        return inventoryType;
+    }
+
+    public void setInventoryType(String inventoryType) {
+        this.inventoryType = inventoryType;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public int getRequiredAmount() {
+        return requiredAmount;
+    }
+
+    public void setRequiredAmount(int requiredAmount) {
+        this.requiredAmount = requiredAmount;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + Objects.hashCode(this.inventoryType);
+        hash = 83 * hash + this.quantityInStock;
+        hash = 83 * hash + this.requiredAmount;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InventoryItem other = (InventoryItem) obj;
+        if (this.quantityInStock != other.quantityInStock) {
+            return false;
+        }
+        if (this.requiredAmount != other.requiredAmount) {
+            return false;
+        }
+        if (!Objects.equals(this.inventoryType, other.inventoryType)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
