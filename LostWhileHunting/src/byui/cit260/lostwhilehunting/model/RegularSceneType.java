57b14e031368752dfa78ee1e33148ff323d34c2e
Felicia Warner <war16035@byui.edu>
2017-11-17 16:13:31 -0500

    17/11/17 started implementing createScenes() function

--- LostWhileHunting/src/byui/cit260/lostwhilehunting/model/RegularSceneType.java
+++ LostWhileHunting/src/byui/cit260/lostwhilehunting/model/RegularSceneType.java
@@ -49,73 +49,8 @@ public class RegularSceneType implements Serializable {
     public void setSafe(boolean safe) {
         this.safe = safe;
     }
-    public static RegularSceneType[] createScenes() {
-        RegularSceneType[] scenesContainer = new RegularSceneType[10];
-        
-        RegularSceneType startingArea = new RegularSceneType();
-        startingArea.nameOfScene = "Starting Area";
-        startingArea.description = "This is where the player will start.";
-        startingArea.safe = true;
-        scenesContainer[0] = startingArea;
-        
-        RegularSceneType coolStream = new RegularSceneType();
-        coolStream.nameOfScene = "Cool Stream";
-        coolStream.description = "The player can be refreshed here.";
-        coolStream.safe = true;
-        scenesContainer[1] = coolStream;
-        
-        RegularSceneType cave = new RegularSceneType();
-        cave.nameOfScene = "Cave";
-        cave.description = "Player can rest in this location";
-        cave.safe = true;
-        scenesContainer[2] = cave;
-        
-        RegularSceneType deepCavern = new RegularSceneType();
-        deepCavern.nameOfScene = "Deep Caverns";
-        deepCavern.description = "The player has to be careful in this location.";
-        deepCavern.safe = false;
-        scenesContainer[3] = deepCavern;
-        
-        RegularSceneType greatRiver = new RegularSceneType();
-        greatRiver.nameOfScene = "Great River";
-        greatRiver.description = "Danger.  Player must beware.";
-        greatRiver.safe = false;
-        scenesContainer[4] = greatRiver;
-        
-        RegularSceneType smallTown = new RegularSceneType();
-        smallTown.nameOfScene = "Small Town";
-        smallTown.description = "The player can trade items they have for other items.";
-        smallTown.safe = true;
-        scenesContainer[5] = smallTown;
-        
-        RegularSceneType goal = new RegularSceneType();
-        goal.nameOfScene = "Goal";
-        goal.description = "player is successful, the game is complete.";
-        goal.safe = false;
-        scenesContainer[6] = goal;
-        
-        RegularSceneType minor1 = new RegularSceneType();
-        minor1.nameOfScene = "Minor 1";
-        minor1.description = "The player can encounter Coyote or Wolf.";
-        minor1.safe = false;
-        scenesContainer[7] = minor1;
-        
-        RegularSceneType minor2 = new RegularSceneType();
-        minor2.nameOfScene = "Minor 2";
-        minor2.description = "The player can encounter wolf or beer.";
-        minor2.safe = false;
-        scenesContainer[8] = minor2;
-        
-        RegularSceneType minor3 = new RegularSceneType();
-        minor3.nameOfScene = "Minor 3";
-        minor3.description = "The player may encounter beer or muggers.";
-        minor3.safe = false;
-        scenesContainer[9] = minor3;
-        
-        return scenesContainer;
-         
-    }
-     //Class Hash, Equals, and toString override functions
+    
+    //Class Hash, Equals, and toString override functions
 
     @Override
     public int hashCode() {
