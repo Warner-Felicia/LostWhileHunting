8b27bc5be1b675a41120ca603677306e45e227e8
tlove21 <tlove21@gmail.com>
2017-11-17 15:39:02 -0500

    17/11/2017 - Scenes Type Enum Created

---
+++ LostWhileHunting/src/byui/cit260/lostwhilehunting/model/SceneType.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lostwhilehunting.model;
+
+/**
+ *
+ * @author Administrator
+ */
+public enum SceneType {
+    startingArea,
+    coolStream,
+    cave,
+    deepCaverns,
+    greatRiver,
+    smallTown,
+    goal,
+    minor1,
+    minor2,
+    minor3
+}
